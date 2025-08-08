165990a486532c7f46e07a6585c4fd7f7724d5e8
deepc <deepanshu.chalia96@gmail.com>
2025-08-08 15:31:10 -0500

    added store

---
+++ storefront/playground/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from store.models import Collection, Product
+
+def say_hello(request):
+  
+  collection = Collection(pk=11)
+  collection.title = 'Games'
+  collection.featured_product = None
+  collection.save()
+  
+  return render(request, 'hello.html', {'name': 'Deepanshu'})
