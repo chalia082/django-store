6915eaf815a2f80a6b1c08ba1098d0f9d3f8e55a
deepc <deepanshu.chalia96@gmail.com>
2025-08-10 08:37:39 -0500

    added review (view, serializer, url) and pagination

--- storefront/store/urls.py
+++ storefront/store/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 from .import views
 
-from rest_framework.routers import SimpleRouter
+from rest_framework_nested import routers
 
-router = SimpleRouter()
-router.register('products', views.ProductViewSet)
+router = routers.DefaultRouter()
+router.register('products', views.ProductViewSet, basename='products')
 router.register('collections', views.CollectionViewSet)
 
-urlpatterns = router.urls
+products_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
+products_router.register('reviews', views.ReviewViewSet, basename='product-reviews')
+
+
+urlpatterns = router.urls + products_router.urls
 
 
 # urlpatterns = [
--- storefront/store/views.py
+++ storefront/store/views.py
@@ -1,21 +1,26 @@
-from .serializers import ProductSerializer, CollectionSerializer
-from django.shortcuts import get_object_or_404
-
-from .models import OrderItem, Product, Collection
 from django.db.models import Count
+from django_filters.rest_framework import DjangoFilterBackend
 
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.mixins import ListModelMixin, CreateModelMixin
-from rest_framework.viewsets import ModelViewSet
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework import status
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
+
+from .filters import ProductFilter
+from .models import OrderItem, Product, Collection, Review
+from .pagination import DefaultPagination
+from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
+
 
 class ProductViewSet(ModelViewSet):
-  
   queryset = Product.objects.all()
   serializer_class = ProductSerializer
+  filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+  filterset_class = ProductFilter
+  pagination_class = DefaultPagination
+  search_fields = ['title', 'description']
+  ordering_fields = ['unit_price', 'last_update']
   
   def get_serializer_context(self):
     return {'request': self.request}
@@ -34,6 +39,15 @@ class CollectionViewSet(ModelViewSet):
     if Collection.objects.filter(featured_product_id=kwargs['pk']).count() > 0:
       return Response({'error': 'Collection cannot be deleted because it includes one or more products.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
     return super().destroy(request, *args, **kwargs)
+  
+class ReviewViewSet(ModelViewSet):
+  serializer_class = ReviewSerializer
+  
+  def get_queryset(self):
+    return Review.objects.filter(product_id=self.kwargs['product_pk'])
+  
+  def get_serializer_context(self):
+    return {'product_id': self.kwargs['product_pk']}
   
   # def delete(self, request, pk):
   #   collection = get_object_or_404(Collection, pk=pk)
