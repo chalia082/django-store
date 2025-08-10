94f8ca5b91f96176ca8751d59c8168400613e59d
deepc <deepanshu.chalia96@gmail.com>
2025-08-10 07:08:42 -0500

    Added generic model view set to simplify code and reduce redudancy. (note - class CollectionViewSet destroy function needs to be refactored to work properly)

--- storefront/store/urls.py
+++ storefront/store/urls.py
@@ -1,10 +1,21 @@
 from django.urls import path
 from .import views
 
-# URLConfig
-urlpatterns = [
-  path('products/', views.product_list),
-  path('collections/', views.collection_list),
-  path('products/<int:id>/', views.product_detail),
-  path('collections/<int:pk>/', views.collection_detail)
-]
+from rest_framework.routers import SimpleRouter
+
+router = SimpleRouter()
+router.register('products', views.ProductViewSet)
+router.register('collections', views.CollectionViewSet)
+
+urlpatterns = router.urls
+
+
+# urlpatterns = [
+#   # path('products/', views.product_list),
+#   # path('products/<int:id>/', views.product_detail),
+#   # path('collections/', views.collection_list),
+#   # path('products/', views.ProductList.as_view()),
+#   # path('collections/', views.CollectionList.as_view()),
+#   # path('products/<int:pk>/', views.ProductDetail.as_view()),
+#   # path('collections/<int:pk>/', views.CollectionDetails.as_view())
+# ]
