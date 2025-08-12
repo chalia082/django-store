e15cd174528f85058bc461b191b8946d1d65ec67
deepc <deepanshu.chalia96@gmail.com>
2025-08-12 06:34:13 -0500

    Added authorization on url requests

--- storefront/store/views.py
+++ storefront/store/views.py
@@ -3,11 +3,14 @@ from django.db.models import Count
 from django_filters.rest_framework import DjangoFilterBackend
 
 from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.mixins import CreateModelMixin, DestroyModelMixin, RetrieveModelMixin, UpdateModelMixin
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 
+from .permissions import IsAdminOrReadOnly
 from .filters import ProductFilter
 from .models import Cart, CartItem, Customer, OrderItem, Product, Collection, Review
 from .pagination import DefaultPagination
@@ -20,6 +23,7 @@ class ProductViewSet(ModelViewSet):
   filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
   filterset_class = ProductFilter
   pagination_class = DefaultPagination
+  permission_classes = [IsAdminOrReadOnly]
   search_fields = ['title', 'description']
   ordering_fields = ['unit_price', 'last_update']
   
@@ -35,6 +39,7 @@ class CollectionViewSet(ModelViewSet):
   queryset = Collection.objects.annotate( 
         products_count=Count('products')).all()
   serializer_class = CollectionSerializer
+  permission_classes = [IsAdminOrReadOnly]
 
   def destroy(self, request, *args, **kwargs):
     if Collection.objects.filter(featured_product_id=kwargs['pk']).count() > 0:
@@ -75,9 +80,19 @@ class ReviewViewSet(ModelViewSet):
   def get_serializer_context(self):
     return {'product_id': self.kwargs['product_pk']}
   
-class CustomerViewSet(CreateModelMixin, 
-                      RetrieveModelMixin, 
-                      UpdateModelMixin,
-                      GenericViewSet):
+class CustomerViewSet(ModelViewSet):
   queryset = Customer.objects.all()
   serializer_class = CustomerSerializer
+  permission_classes = [IsAdminUser]
+  
+  @action(detail=False, methods=['GET', 'PUT'], permission_classes=[IsAuthenticated])
+  def me(self, request):
+    (customer, created) = Customer.objects.get_or_create(user_id=request.user.id)
+    if request.method == 'GET':
+      serializer = CustomerSerializer(customer) 
+      return Response(serializer.data)
+    elif request.method == 'PUT':
+      serializer = CustomerSerializer(customer, data=request.data)
+      serializer.is_valid(raise_exception=True)
+      serializer.save()
+      return Response(serializer.data)
