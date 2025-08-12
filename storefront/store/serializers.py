c8e3d29283097c1b0fd17d222fa26e095a6917b6
deepc <deepanshu.chalia96@gmail.com>
2025-08-12 14:47:55 -0500

    added signals

--- storefront/store/serializers.py
+++ storefront/store/serializers.py
@@ -2,6 +2,7 @@ from decimal import Decimal
 from django.db import transaction
 from rest_framework import serializers
 from .models import Cart, CartItem, Customer, Order, OrderItem, Product, Collection, Review
+from .signals import order_created
 
 class CollectionSerializer(serializers.ModelSerializer):
   class Meta:
@@ -131,7 +132,7 @@ class CreateOrderSerializer(serializers.Serializer):
     with transaction.atomic():
       cart_id = self.validated_data['cart_id']
       
-      (customer, created) = Customer.objects.get_or_create(user_id=self.context['user_id'])
+      customer = Customer.objects.get(user_id=self.context['user_id'])
       order = Order.objects.create(customer=customer)
       
       cart_items = CartItem.objects \
@@ -149,5 +150,7 @@ class CreateOrderSerializer(serializers.Serializer):
       OrderItem.objects.bulk_create(order_items)
       
       Cart.objects.filter(pk=cart_id).delete()
+      
+      order_created.send_robust(self.__class__, order=order)
       
       return order
