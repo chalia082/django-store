324ccf9cc918d5bc35dcf87052718aa1ae0ef78b
deepc <deepanshu.chalia96@gmail.com>
2025-08-22 11:10:25 -0500

    Add Whitenoise middleware to production settings for static files

--- storefront/storefront/settings/prod.py
+++ storefront/storefront/settings/prod.py
@@ -4,6 +4,19 @@ from .common import *
 
 DEBUG = False
 
+# Add Whitenoise middleware to serve static files
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add this for static files
+    'corsheaders.middleware.CorsMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
 SECRET_KEY = os.environ['SECRET_KEY']
 
 ALLOWED_HOSTS = ['deep-storefront-prod-7fff68b53dc6.herokuapp.com']
@@ -33,6 +46,9 @@ STATIC_URL = '/static/'
 
 # Whitenoise for serving static files
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Additional static files configuration
+STATICFILES_DIRS = []
 
 # Email settings (optional - will be set if Mailgun addon is added)
 EMAIL_HOST = os.environ.get('MAILGUN_SMTP_SERVER', '')
